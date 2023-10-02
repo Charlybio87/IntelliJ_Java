10bba56ee627635e055ddaf28829cab1ae7df354
Carlos Alberto Ribas <ribascarlos.a@outlook.com>
2023-10-02 16:42:24 -0300

    initial commit

---
+++ POO/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Alumnos miPrimerObjeto = new Alumnos("Carlos", "Ribas","36");
+        Alumnos miPrimerObjeto = new Alumnos("Marta", "Zalazar","24");
+        System.out.println("hola gente");
+        System.out.println("Manda un nuevo mensaje");
+        System.out.println("hay que ingresar datos");
+
+    }
+}
